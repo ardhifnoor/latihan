16c8e0362510d01141b080eba31fa5e58e62792c
Ardhi <ardhi@Ardhi-2.local>
2019-08-04 13:31:23 +0700

    Add daftar film

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div><h1>Selamat datang</h1></div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      film: []
+    }
+  }
+  componentWillMount(){
+    axios.get('https://facebook.github.io/react-native/movies.json')
+    .then((dataFilm)=>{
+      this.setState({
+        film: dataFilm.data.movies
+      })
+    })
+  };
+
+  render(){
+    function renderDaftarFilm(film) {
+      return film.map(film => {
+        return <li key = {film.id}>{film.title} - {film.releaseYear}</li>
+      })
+    }
+    return(
+      <div>
+        <h1>Ini portofolio saya</h1>
+        <h2>Daftar Film</h2>
+        <ul>{renderDaftarFilm(this.state.film)}</ul>
+      </div>
+    )
+  }
 }
 
 export default App;
